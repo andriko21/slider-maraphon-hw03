979318a5d5b3e083e66372174a2b6de147a70b6e
andriko21 <75207634+andriko21@users.noreply.github.com>
2021-10-04 14:15:21 +0300

    Дрібниці

--- app.js
+++ app.js
@@ -1,38 +1,44 @@
-const uppBtn = document.querySelector('.up-button')
-const downBtn = document.querySelector('.down-button')
-const sideBAr = document.querySelector('.sidebar')
-const mainSlide = document.querySelector('.main-slide')
-const container = document.querySelector('.container')
-const slidesLength = mainSlide.querySelectorAll('div').length
+const uppBtn = document.querySelector(".up-button");
+const downBtn = document.querySelector(".down-button");
+const sideBAr = document.querySelector(".sidebar");
+const mainSlide = document.querySelector(".main-slide");
+const container = document.querySelector(".container");
+const slidesLength = mainSlide.querySelectorAll("div").length;
 
-sideBAr.style.top = `-${(slidesLength - 1) * 100}vh`
+sideBAr.style.top = `-${(slidesLength - 1) * 100}vh`;
 
-let activeSlide = 0
+let activeSlide = 0;
 
-uppBtn.addEventListener('click', () => {
-    changeSlide('up')
-})
+uppBtn.addEventListener("click", () => {
+  changeSlide("up");
+});
 
-downBtn.addEventListener('click', () => {
-    changeSlide('down')
-})
+downBtn.addEventListener("click", () => {
+  changeSlide("down");
+});
+
+document.addEventListener("keydown", (ev) => {
+  if (ev.key === "ArrowUp") {
+    changeSlide("up");
+  } else if (ev.key === "ArrowDown") {
+    changeSlide("down");
+  }
+});
 
 function changeSlide(string) {
-    if (string === 'up') {
-        activeSlide++
-        
-        if (activeSlide === slidesLength) {
-            activeSlide = 0
-        }
-    } else if (string === 'down') {
-        activeSlide--
-        if (activeSlide < 0) {
-            activeSlide = slidesLength -1
-        }
+  if (string === "up") {
+    activeSlide++;
+
+    if (activeSlide === slidesLength) {
+      activeSlide = 0;
     }
-    const height = container.clientHeight
-    mainSlide.style.transform = `translateY(-${activeSlide * height}px)`
-    sideBAr.style.transform = `translateY(${activeSlide * height}px)`
-
+  } else if (string === "down") {
+    activeSlide--;
+    if (activeSlide < 0) {
+      activeSlide = slidesLength - 1;
+    }
+  }
+  const height = container.clientHeight;
+  mainSlide.style.transform = `translateY(-${activeSlide * height}px)`;
+  sideBAr.style.transform = `translateY(${activeSlide * height}px)`;
 }
-
