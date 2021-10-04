7fa37f3f5cf55c1f1d40ddb16b3db756b6abdc1a
andriko21 <75207634+andriko21@users.noreply.github.com>
2021-10-04 13:53:42 +0300

    Все

---
+++ app.js
@@ -0,0 +1,38 @@
+const uppBtn = document.querySelector('.up-button')
+const downBtn = document.querySelector('.down-button')
+const sideBAr = document.querySelector('.sidebar')
+const mainSlide = document.querySelector('.main-slide')
+const container = document.querySelector('.container')
+const slidesLength = mainSlide.querySelectorAll('div').length
+
+sideBAr.style.top = `-${(slidesLength - 1) * 100}vh`
+
+let activeSlide = 0
+
+uppBtn.addEventListener('click', () => {
+    changeSlide('up')
+})
+
+downBtn.addEventListener('click', () => {
+    changeSlide('down')
+})
+
+function changeSlide(string) {
+    if (string === 'up') {
+        activeSlide++
+        
+        if (activeSlide === slidesLength) {
+            activeSlide = 0
+        }
+    } else if (string === 'down') {
+        activeSlide--
+        if (activeSlide < 0) {
+            activeSlide = slidesLength -1
+        }
+    }
+    const height = container.clientHeight
+    mainSlide.style.transform = `translateY(-${activeSlide * height}px)`
+    sideBAr.style.transform = `translateY(${activeSlide * height}px)`
+
+}
+
